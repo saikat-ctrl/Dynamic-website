3c90f2372ce2bf114d54daa66978de2619e636a5
saikat-ctrl <saikatray1999@gmail.com>
2025-07-07 18:06:43 +0530

    error1

--- src/app.js
+++ src/app.js
@@ -156,8 +156,8 @@ app.post("/register", async (req, res) => {
 app.get("/dashboard", auth, (req, res) => {
   res.status(200).render("dashboard", { user: req.user });
 });
-app.get("/504", (req, res) => {
-  res.status(504).render("504");
+app.get("/error", (req, res) => {
+  res.status(504).render("error", { message: "504 Gateway Timeout" });
 });
 
 // Start server
